4c9cf0d281d17a81ff8196c3531a98698288a8fd
Leonardo Bollazzi <leonardo.bollazzi@students.fhnw.ch>
2022-04-25 17:50:35 +0200

    All repositories

--- src/main/java/ch/fhnw/acrm/data/repository/OrderRepository.java
+++ src/main/java/ch/fhnw/acrm/data/repository/OrderRepository.java
@@ -2,8 +2,13 @@ package ch.fhnw.acrm.data.repository;
 
 import ch.fhnw.acrm.data.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query("select o from Order o where o.id = ?1")
+    Order FindByID(Long id);
+
 }
--- src/main/java/ch/fhnw/acrm/data/repository/Order_ItemRepository.java
+++ src/main/java/ch/fhnw/acrm/data/repository/Order_ItemRepository.java
@@ -2,8 +2,21 @@ package ch.fhnw.acrm.data.repository;
 
 import ch.fhnw.acrm.data.domain.Order_Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface Order_ItemRepository extends JpaRepository<Order_Item, Long> {
+
+    @Query("select count(o) from Order_Item o where o.order.id = ?1")
+    long CountItem(Long id);
+
+    @Query("select o.product.id from Order_Item o where o.order.id = ?1")
+    List<Order_Item> findOrderItemByOrder(Long id);
+
+    @Query("select o from Order_Item o where o.order.agent.id = ?1")
+    List<Order_Item> findOrderItemByAgent(Long id);
+
 }
